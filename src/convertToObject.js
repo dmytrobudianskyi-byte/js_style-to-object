3b06f5055c1aafb13f17486f6dd6c6e56fa25d47
Dmytro Budianskyi <dmytro.budianskyi@gmail.com>
2026-09-17 12:46:06 +0200

    completed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styleObj = {};
+
+  for (const styleLine of sourceString.split(';')) {
+    if (styleLine.trim().length != 0) {
+      const [key, value] = styleLine.split(':');
+
+      styleObj[key.trim()] = value.trim();
+    }
+  }
+
+  console.log(styleObj == { position: 'absolute' });
+  console.log(styleObj);
+  console.log({ position: 'absolute' });
+
+  return styleObj;
 }
 
 module.exports = convertToObject;
